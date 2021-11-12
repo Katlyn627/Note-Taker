a46e111ffacb9e6e7ad23e5264871b103ce2b006
Katlyn Boches <katlynboches@gmail.com>
2021-11-12 15:15:13 -0700

    updated js files and reworked code

---
+++ routes/routes.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = app => {
+
+    // Setup notes variable
+    fs.readFile("db/db.json","utf8", (err, data) => {
+
+        if (err) throw err;
+
+        var notes = JSON.parse(data);
+    
+        // Setup the API and notes routes
+        app.get("/api/notes", function(req, res) {
+            // Read the db.json file and return all saved notes as JSON.
+            res.json(notes);
+        });
+        app.post("/api/notes", function(req, res) {
+            // Receives a new note, adds it to db.json, then returns the new note
+            let newNote = req.body;
+            notes.push(newNote);
+            updateDb();
+            return console.log("Added new note: "+newNote.title);
+        });
+
+        // Retrieves a note with specific id
+        app.get("/api/notes/:id", function(req,res) {
+            // display json for the notes array indices of the provided id
+            res.json(notes[req.params.id]);
+        });
+
+        // Deletes a note with specific id
+        app.delete("/api/notes/:id", function(req, res) {
+            notes.splice(req.params.id, 1);
+            updateDb();
+            console.log("Deleted note with id "+req.params.id);
+        });
+
+        // Display notes.html when /notes is accessed
+        app.get('/notes', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+        
+        // Display index.html when all other routes are accessed
+        app.get('*', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
+
+        //Updates the json file whenever a note is added or deleted
+        function updateDb() {
+            fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+                if (err) throw err;
+                return true;
+            });
+        }
+
+    });
+
+}
--- server.js
+++ server.js
@@ -1,26 +1,21 @@
-// Dependencies
-// =============================================================
+// Require Dependencies
 const express = require("express");
 const fs = require("fs");
+const path = require('path');
 
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080
-
-// Sets up the Express app to handle data parsing
-// =============================================================
+// Setup data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/assets", express.static("./assets"));
+app.use(express.static(__dirname));
 
+//Require routes file
+require('./routes/routes')(app);
 
-require("./routes/htmlroutes")(app);
-require("./routes/apiroutes")(app);
-
-// Starts the server to begin listening
-// =============================================================
+// Setup listener
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+    console.log("App listening on PORT: " + PORT);
+});  
