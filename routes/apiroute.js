84b604088b1136b040cbb6a5d203059f1500dde2
Katlyn Boches <katlynboches@gmail.com>
2021-11-12 10:02:39 -0700

    Created api and HTML functions to get routes

---
+++ routes/apiroute.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+
+module.exports = function(app) {
+  // Created routes one to the index page and one to the notes page
+  app.get('/api/notes', function(req, res) {
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      res.send(dbData);
+    });
+  });
+
+  // Created API post request
+
+  app.post('/api/notes', function(req, res) {
+    const userNotes = req.body;
+
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      dbData.push(userNotes);
+      let number = 1;
+      dbData.forEach((note, index) => {
+        note.id = number;
+        number++;
+        return dbData;
+      });
+      console.log(dbData);
+
+      stringData = JSON.stringify(dbData);
+
+      fs.writeFile('./db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    res.send('Thank you for your note!');
+  });
+
+  // API route that allows user to delete a note 
+  app.delete('/api/notes/:id', function(req, res) {
+    // Gets id of the note to be deleted
+    const deleteNote = req.params.id;
+    console.log(deleteNote);
+
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+
+      // Compares each ID to the note to be deleted
+      dbData = JSON.parse(data);
+      for (let i = 0; i < dbData.length; i++) {
+        if (dbData[i].id === Number(deleteNote)) {
+          dbData.splice([i], 1);
+        }
+      }
+      console.log(dbData);
+      stringData = JSON.stringify(dbData);
+
+      fs.writeFile('./db/db.json', stringData, (err, data) => {
+        if (err) throw err;
+      });
+    });
+    // Express response.status(204)
+    res.status(204).send();
+  });
+};
